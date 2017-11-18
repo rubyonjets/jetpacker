7533a9ef9fa66c75cb470b1108081da415536727
Tung Nguyen <tongueroo@gmail.com>
2017-11-17 23:23:03 -0800

    require rails core_ext

--- lib/webpacker.rb
+++ lib/webpacker.rb
@@ -1,3 +1,8 @@
+# webpack relies on rails which uses these core_ext but since
+# we're not entirely requiring rails we need to require all the core_ext
+# individually, yeah a bit of a pain
+require "active_support/core_ext/object"
+require "active_support/core_ext/hash"
 require "active_support/core_ext/module/attribute_accessors"
 require "active_support/logger"
 require "active_support/tagged_logging"
