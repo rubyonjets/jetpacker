84006c99dc39b6be3e874ab138c8ae125a32224b
Tung Nguyen <tongueroo@gmail.com>
2018-10-31 13:50:40 +0000

    update patch version

--- lib/webpacker/version.rb
+++ lib/webpacker/version.rb
@@ -1,4 +1,9 @@
 module Webpacker
   # Change the version in package.json too, please!
-  VERSION = "3.2.0".freeze
+  # Since this fork and branch originally started from rails/webpacker
+  # The git tags collide. As a hacky workaround adding 100 to the patch version.
+  # Example:
+  #  webpacker-jets 3.2.100 # added 100 to the patch version
+  #  webpacker 3.2.0 # original
+  VERSION = "3.2.100".freeze
 end
